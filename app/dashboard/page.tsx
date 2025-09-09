a15fd20caea98628fbc913f706aaa99afce8599b
Nayan Gallego Sivaraman <nayan@Vijays-MacBook-Pro.local>
2025-09-10 03:25:47 +1000

    added dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1>LOGIN WORKS!!!!</h1>
+    </div>
+  );
+}
