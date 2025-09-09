6c8c228f9d9b50f08ac3c8337b7b1eb810bb07b4
Nayan Gallego Sivaraman <nayan@Vijays-MacBook-Pro.local>
2025-09-10 04:15:13 +1000

    formatting login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,11 +11,12 @@ export default function Login() {
     const [password, setPassword] = useState("");
     const [password2, setPassword2] = useState("");
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
     const router = useRouter();
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-    
+        setLoading(true);
         const result = await signIn("credentials", {
           redirect: false,
           email,
@@ -27,6 +28,7 @@ export default function Login() {
         } else {
         router.push("/dashboard"); // redirect after successful login
         }
+      setLoading(false);
     };
 
     return (
@@ -37,8 +39,9 @@ export default function Login() {
             >
             Back
         </Link>
+        {error && <p style={{ color: "red" }}>{error}</p>}
         <form onSubmit={handleSubmit} className="gap-2">
-          <div className="relative bg-gray-200 dark:bg-gray-700 rounded-lg shadow-lg w-96 h-64 flex flex-col items-center justify-centerm gap-6">
+          <div className="relative bg-gray-200 dark:bg-gray-700 rounded-lg shadow-lg w-96 h-70 flex flex-col items-center justify-centerm gap-6">
             <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-100">Log In</h1>
               <input
                 type="email"
@@ -58,12 +61,11 @@ export default function Login() {
               />
               <button 
               type="submit" 
-              disabled={!email || !password}
+              disabled={!email || !password || loading}
               className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 disabled:bg-gray-400"
               >
-              Log In
+              {loading ? "Logging in..." : "Log in"}
               </button>
-              {error && <p style={{ color: "red" }}>{error}</p>}
             </div>
         </form>
           <Link href="/signup" className="hover:underline">Don&apos;t have an account yet?{" "}Click here to sign up.</Link>
