40ad927efc3421ecfbbc95b964c8cd0d177c9b92
Nayan Gallego Sivaraman <nayan@Vijays-MacBook-Pro.local>
2025-09-10 21:18:21 +1000

    client stats

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -30,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionProvider>{children}</SessionProvider>
+        {children}
       </body>
     </html>
   );
